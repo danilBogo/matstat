66504c7c17fba96623dac22560d2b9b586e1edf5
danilBogo <danil.bogo@gmail.com>
2022-04-27 15:23:46 +0300

    [add] parser

---
+++ MathStatApp/MathStatApp/Parser.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using CsvHelper;
+
+namespace MathStatApp;
+
+public class Parser
+{
+    private readonly string _pathFile;
+    public Parser(string pathFile)
+    {
+        _pathFile = pathFile;
+    }
+
+    public IEnumerable<T> GetData<T>()
+    {
+        using var reader = new StreamReader(_pathFile);
+        using var csv = new CsvReader(reader, CultureInfo.CurrentCulture);
+        var records = csv.GetRecords<T>();
+        foreach (var data in records)
+        {
+            if (data is not null)
+                yield return data;
+        }
+    }
+}
